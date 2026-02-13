e52308d784172ca389f333334df95aadfe17ddd6
OlMinJe <Ol.MinJe@gmail.com>
2026-02-13 14:34:03 +0900

    💄STYLE: provider 코드 위치 app > _provider로 변경 #18

--- src/app/_providers/QueryProvider.tsx
+++ src/app/_providers/QueryProvider.tsx
@@ -1,8 +1,8 @@
 'use client'
 
+import { queryClient as baseClient } from '@/app/_providers/queryClient'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
-import { queryClient as baseClient } from './queryClient'
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
   const [client] = useState(() => baseClient)
