986461b792bcbf6be2e7945d5afc0363968dc765
OlMinJe <Ol.MinJe@gmail.com>
2026-02-25 16:13:07 +0900

    ♻️ Refactor: 테스트용 파일 mock으로 이동 및 예시 코드 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-'use client'
-
-import { createClient } from '@/shared/lib/supabase/client'
-import { Button } from '@/shared/ui/Button'
-import { useQuery } from '@tanstack/react-query'
-
-export default function Home() {
-  const { data } = useQuery({
-    queryKey: ['test'],
-    queryFn: async () => 'react-query-ok',
-  })
-
-  const signOut = async () => {
-    const supabase = createClient()
-    await supabase.auth.signOut()
-
-    const { data } = await supabase.auth.getSession()
-    console.log(data.session) // null이면 로그아웃 OK
-    location.href = '/auth'
-  }
-
-  return <Button onClick={signOut}>로그아웃</Button>
+export default function Page() {
+  return <>메인 페이지 입니다. 테스트용 코드는 Mock 폴더에 있어요</>
 }
