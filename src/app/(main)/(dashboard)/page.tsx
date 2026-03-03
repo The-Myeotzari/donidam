40e3bef5d4ac943b5fd78ea4801b5459951ca24f
OlMinJe <Ol.MinJe@gmail.com>
2026-03-03 17:13:19 +0900

    ✨ Feat: 서버 프리패칭으로 데이터 미리 채워서 대시보드 메인 카드에 전달 #55

--- src/app/(main)/(dashboard)/page.tsx
+++ src/app/(main)/(dashboard)/page.tsx
@@ -1,9 +1,29 @@
+import { fetchMainDashboardCard } from '@/entities/dashboard-card/api/dashboardCard.queries'
 import { DashboardCard } from '@/entities/dashboard-card/ui/DashboardCard'
+import { DashboardCardSkeleton } from '@/entities/dashboard-card/ui/DashboardCardSkeleton'
+import { QUERY_KEYS } from '@/shared/constants/queryKey'
+import { getCookies } from '@/shared/lib/api/getCookies'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
+import { Suspense } from 'react'
 
-export default function Page() {
+export default async function Page() {
+  const queryClient = new QueryClient()
+  const cookie = await getCookies()
+
+  await queryClient.prefetchQuery({
+    queryKey: QUERY_KEYS.DASHBOARD.mainCard(),
+    queryFn: () => fetchMainDashboardCard(undefined, { headers: { Cookie: cookie } }),
+  })
+
   return (
     <>
-      <DashboardCard />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<DashboardCardSkeleton />}>
+          <DashboardCard />
+        </Suspense>
+      </HydrationBoundary>
+
+      <div>버튼 리스트</div>
     </>
   )
 }
