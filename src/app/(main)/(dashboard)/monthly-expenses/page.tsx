49bcc4958566c42a8bb7e09c88f7d5d74ea02a39
OlMinJe <Ol.MinJe@gmail.com>
2026-03-05 13:44:09 +0900

    ✨ Feat: 카테고리별 지출 카드 페이지에 prefatch로 import #55

--- src/app/(main)/(dashboard)/monthly-expenses/page.tsx
+++ src/app/(main)/(dashboard)/monthly-expenses/page.tsx
@@ -3,6 +3,8 @@ import { QUERY_KEYS } from '@/shared/constants/queryKey'
 import { getCookies } from '@/shared/lib/api/getCookies'
 import { BudgetStatus } from '@/widgets/monthly-expense-budget-status/ui/BudgetStatus'
 import { BudgetStatusSkeleton } from '@/widgets/monthly-expense-budget-status/ui/BudgetStatusSkeleton'
+import { SpendingByCategory } from '@/widgets/monthly-expense-spending-by-category/ui/SpendingByCategory'
+import { SpendingByCategorySkeleton } from '@/widgets/monthly-expense-spending-by-category/ui/SpendingByCategorySkeleton'
 import { MonthlyExpenseSummary } from '@/widgets/monthly-expense/ui/MonthlyExpenseSummary'
 import { MonthlyExpenseSummarySkeleton } from '@/widgets/monthly-expense/ui/MonthlyExpenseSummarySkeleton'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
@@ -29,6 +31,12 @@ export default async function MonthlyExpensesPage() {
       <HydrationBoundary state={dehydrate(queryClient)}>
         <Suspense fallback={<BudgetStatusSkeleton />}>
           <BudgetStatus />
+        </Suspense>
+      </HydrationBoundary>
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<SpendingByCategorySkeleton />}>
+          <SpendingByCategory />
         </Suspense>
       </HydrationBoundary>
     </>
