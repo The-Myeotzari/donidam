376135723929d78ab013091b018eba941d32da02
OlMinJe <Ol.MinJe@gmail.com>
2026-03-11 14:44:20 +0900

    ✨Feat: 결제 수단 페이지 구현 #69

--- src/app/(main)/menu/settings/card/page.tsx
+++ src/app/(main)/menu/settings/card/page.tsx
@@ -1,5 +1,21 @@
+import { fetchPaymentMethods } from '@/entities/payment-method/api/fetchPaymentMethods.api'
+import { QUERY_KEYS } from '@/shared/constants/queryKey'
+import { getCookies } from '@/shared/lib/api/getCookies'
 import { CardSettingsWidget } from '@/widgets/payment-method-list/ui/CardSettingsWidget'
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query'
 
-export default function CardPage() {
-  return <CardSettingsWidget />
+export default async function CardPage() {
+  const queryClient = new QueryClient()
+  const cookie = await getCookies()
+
+  await queryClient.prefetchQuery({
+    queryKey: QUERY_KEYS.PAYMENT_METHODS.list(),
+    queryFn: () => fetchPaymentMethods({ headers: { Cookie: cookie } }),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <CardSettingsWidget />
+    </HydrationBoundary>
+  )
 }
