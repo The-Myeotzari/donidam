cbfcba29e385ec3a4239003eff57e9d287f68e0b
OlMinJe <Ol.MinJe@gmail.com>
2026-03-11 14:32:11 +0900

    🗃️ DB: payment_methods 테이블 수정 #69

--- src/shared/types/database.types.ts
+++ src/shared/types/database.types.ts
@@ -291,7 +291,7 @@ export type Database = {
           is_nodam_active?: boolean
           monthly_budget?: number
           role?: Database['public']['Enums']['user_role'] | null
-          app_pin: string | null
+          app_pin?: string | null
         }
         Update: {
           auto_allowance_amount?: number | null
@@ -302,7 +302,7 @@ export type Database = {
           is_nodam_active?: boolean
           monthly_budget?: number
           role?: Database['public']['Enums']['user_role'] | null
-          app_pin: string | null
+          app_pin?: string | null
         }
         Relationships: []
       }
