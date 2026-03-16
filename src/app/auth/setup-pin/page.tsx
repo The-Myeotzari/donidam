22ba009a6f377cd010d50e9f0b85beb67565fbcd
OlMinJe <Ol.MinJe@gmail.com>
2026-03-16 13:27:49 +0900

    ✨ Feat: PIN 설정 페이지 추가 #84

---
+++ src/app/auth/setup-pin/page.tsx
@@ -0,0 +1,9 @@
+import { SetupPinForm } from '@/features/setup-pin/ui/SetupPinForm'
+
+export default function SetupPinPage() {
+  return (
+    <div className="min-h-screen px-8 pb-8">
+      <SetupPinForm />
+    </div>
+  )
+}
